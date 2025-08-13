26c358360a15f83a4c775b116f076bb6c47ef134
brijesh.nishad <bnlv1212@gmail.com>
2025-08-13 04:43:22 -0700

    corexfin-eureka-server:configured

---
+++ corexfin-registry-server/src/main/java/com/corexfin/CorexfinRegistryServerApplication.java
@@ -0,0 +1,13 @@
+package com.corexfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorexfinRegistryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorexfinRegistryServerApplication.class, args);
+	}
+
+}
